5873b976f172a3f34b534601a2fb494162878fe5
melanipadilla <melanicarolpadillaolivares@MacBook-Air-de-melani.local>
2026-05-03 13:37:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Módulo 3 - Introducción a JavaScript
+
+Este repositorio contiene los ejercicios y prácticas del modulo 3.
+
+## Cómo ejecutar los archivos
+Puedes ejecutar los archivos de JavaScript utilizando Node.js en tu terminal:
+```bash
+node tipos-de-dato.js
+```
---
+++ tipo-de-dato.js
@@ -0,0 +1,29 @@
+// Ejercicio: Tipos de Datos en JavaScript
+
+console.log(typeof 42);
+
+console.log(typeof 'Veinticinco');
+
+console.log(typeof -666);
+
+console.log(typeof true);
+
+console.log(typeof 0);
+
+console.log(typeof '');
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof FALSE);
+
+
+
+console.log(typeof 9007199254740991n);
+
+console.log(typeof { nombre: "Mel", edad: 31 });
+
+console.log(typeof [1, 2, 3]);
+
+console.log(typeof function () { }); 
